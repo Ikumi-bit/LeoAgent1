75a25bf656c22538bf9a3adc8f9a6c0a2b237316
Ikumi-bit <koshikibuno714@gmail.com>
2026-03-19 22:55:40 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Leo is awake."
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    return "OK"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
